4015dc8458c331d4eb23315d4721ef6968459e24
Rasa <therain@rasas-air.home>
2019-05-31 14:31:58 -0400

    Art.java updated to match style guidelines

--- projects/Project1/Art.java
+++ projects/Project1/Art.java
@@ -3,9 +3,6 @@
  *
  * @author Thomas Tillis
  */
-
-import java.util.*;
-
 public class Art {
     /**
      * Starts the program.
@@ -19,69 +16,76 @@ public class Art {
 
     public static void main(String[] args) {
         printCromulon("Show me what you got.");
-        System.out.println();
-        printEarthPlayingMusic("Taylor Swift");
-        System.out.println();
+        printEarthPlayingMusic();
         printCromulon("DISQUALIFIED!");
-        System.out.println();
         printExplosion();
 
     }
 
     /**
-     * Starts the program.
+     * Prints a cromulon (big head) with a speech bubble defined by param
      * 
      * @param cromulonSayWhat String that defines what the cromulon (big head) says
      */
 
-        public static void printCromulon(String cromulonSayWhat) {
-            System.out.println("        ___          ");
-            System.out.println("    . -^   `--,      ");
-            System.out.println("   /# =========`-_   ");
-            System.out.println("  /# (--====___====\\ ");
-            System.out.println(" /#   .- --.  . --.| ");
-            System.out.println("/##   |  * ) (   * ),");
-            System.out.println("|##   \\    /\\ \\   / |");
-            System.out.println("|###   ---   \\ ---  |");
-            System.out.println("|####      ___)    #|    _______________________");
-            System.out.println("|######           ##|  /                        |");
-            //Uses printf to ensure that the speech bubble lines up regardless of text
-            System.out.printf("%-47s"," \\##### ---------- /  < \"" + cromulonSayWhat + "\"");
-            System.out.print(" |\n");
-            System.out.println("  \\####           (    \\________________________|");
-            System.out.println("   `\\###          |  ");
-            System.out.println("     \\###         |  ");
-            System.out.println("      \\##        |   ");
-            System.out.println("       \\###.    .)   ");
-            System.out.println("        `======/     ");
+    public static void printCromulon(String cromulonSayWhat) {
+        System.out.println();
+        System.out.println("        ___          ");
+        System.out.println("    . -^   `--,      ");
+        System.out.println("   /# =========`-_   ");
+        System.out.println("  /# (--====___====\\ ");
+        System.out.println(" /#   .- --.  . --.| ");
+        System.out.println("/##   |  * ) (   * ),");
+        System.out.println("|##   \\    /\\ \\   / |");
+        System.out.println("|###   ---   \\ ---  |");
+        System.out.println("|####      ___)    #|    _______________________");
+        System.out.println("|######           ##|  /                        |");
+        //Uses printf to ensure that the speech bubble lines up regardless of text
+        System.out.printf("%-47s"," \\##### ---------- /  < \"" + cromulonSayWhat + "\"");
+        System.out.print(" |\n");
+        System.out.println("  \\####           (    \\________________________|");
+        System.out.println("   `\\###          |  ");
+        System.out.println("     \\###         |  ");
+        System.out.println("      \\##        |   ");
+        System.out.println("       \\###.    .)   ");
+        System.out.println("        `======/     ");
     }
-        public static void printEarthPlayingMusic(String yourFavoriteArtist) {
-            System.out.println("           _____       ");
-            System.out.println("        .-'.  ':'-.    ");
-            System.out.println("      .''::: .:    '.  ");
-            System.out.println("     /   :::::'      \\ "); 
-            System.out.println("    ;.    ':' `       ;    _______________________");
-            System.out.println("    |       '..       |   /                       |");
-            System.out.println("    ;'        ::::.   ;  <  *Taylor Swift playing |");
-            System.out.println("     \\       '::::   /    \\_______________________|");
-            System.out.println("      '.      :::  .'  ");
-            System.out.println("        '-.___'_.-'    ");
+
+    /**
+     * Prints an ascii earth playing music
+     */
+
+    public static void printEarthPlayingMusic() {
+        System.out.println();
+        System.out.println("           _____       ");
+        System.out.println("        .-'.  ':'-.    ");
+        System.out.println("      .''::: .:    '.  ");
+        System.out.println("     /   :::::'      \\ "); 
+        System.out.println("    ;.    ':' `       ;    _______________________");
+        System.out.println("    |       '..       |   /                       |");
+        System.out.println("    ;'        ::::.   ;  <  *Taylor Swift playing |");
+        System.out.println("     \\       '::::   /    \\_______________________|");
+        System.out.println("      '.      :::  .'  ");
+        System.out.println("        '-.___'_.-'    ");
     }
+    /**
+     * Prints an ascii explosion
+     */
 
     public static void printExplosion(){
-    System.out.println("       _.-^^---....,,--       ");
-    System.out.println("   _--                  --_   ");
-    System.out.println("  <                        >) ");
-    System.out.println("  |                         | ");
-    System.out.println("   \\._                   _./  ");
-    System.out.println("      ```--. . , ; .--'''     ");  
-    System.out.println("            | |   |           ");
-    System.out.println("         .-=||  | |=-.        ");
-    System.out.println("         `-=#$%&%$#=-'        ");
-    System.out.println("            | ;  :|           ");
-    System.out.println("   _____.,-#%&$@%#&#~,._____  ");
-    System.out.println();
-
+        System.out.println();
+        System.out.println("       _.-^^---....,,--       ");
+        System.out.println("   _--                  --_   ");
+        System.out.println("  <                        >) ");
+        System.out.println("  |                         | ");
+        System.out.println("   \\._                   _./  ");
+        System.out.println("      ```--. . , ; .--'''     ");  
+        System.out.println("            | |   |           ");
+        System.out.println("         .-=||  | |=-.        ");
+        System.out.println("         `-=#$%&%$#=-'        ");
+        System.out.println("            | ;  :|           ");
+        System.out.println("   _____.,-#%&$@%#&#~,._____  ");
+        System.out.println();
     }
 
 }
