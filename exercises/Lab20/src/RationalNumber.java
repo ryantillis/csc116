799f3fb0d2cece7c87632f9feb9a731737b438b8
Ziggy Stardust <zig@Judy-Garland.local>
2019-07-12 19:17:23 -0400

    Lab20 completed

--- exercises/Lab20/src/RationalNumber.java
+++ exercises/Lab20/src/RationalNumber.java
@@ -2,7 +2,7 @@
  * @author Thomas Tillis
  */
 
- public class RationalNumber {
+public class RationalNumber {
     /** Numerator of rational number */
     private int numerator;
     /** Denominator of rational number */
@@ -14,6 +14,7 @@
      * cannot be 0, so throw an IllegalArgumentException if 0 is passed.
      * @param numerator integer numerator
      * @param denominator integer denominator
+     * @throws IllegalArgumentException if denominator is zero
      */
     public RationalNumber(int numerator, int denominator) throws IllegalArgumentException {
         if(denominator == 0) {throw new IllegalArgumentException("Denominator is 0"); }
@@ -22,8 +23,8 @@
             this.numerator = (-1) * Math.abs(numerator) / gcd;
             this.denominator = Math.abs(denominator) / gcd;
         } else {
-            this.numerator = numerator / gcd;
-            this.denominator = denominator / gcd;
+            this.numerator = Math.abs(numerator) / gcd;
+            this.denominator = Math.abs(denominator) / gcd;
         }
     }
 
@@ -35,23 +36,32 @@
         this.denominator = 1;
     }
 
-    /** Returns this rational number’s denominator value. */
+    /** Returns this rational number’s denominator value. 
+     * @return integer denominator
+    */
     public int getDenominator(){
         return denominator;
     }
 
-    /** Returns this rational number’s numerator value. */
+    /** Returns this rational number’s numerator value. 
+     * @return integer numerator
+    */
     public int getNumerator() {
         return numerator;
     }
 
-    /** Returns a String representation of this rational number. */
+    /** Returns a String representation of this rational number. 
+     * @return String rational number ratio
+    */
     public String toString() {
         if(this.denominator == 1) { return Integer.toString(numerator); }
         else { return numerator + "/" + denominator; }
     }
 
-    /** Returns true if this and other are equivalent RationalNumbers */
+    /** Returns true if this and other are equivalent RationalNumbers 
+     * @param other Object to compare equality
+     * @return boolean if object is equal
+    */
     public boolean equals(Object other) {
         if(other instanceof RationalNumber) {
             RationalNumber that = (RationalNumber) other;
@@ -63,28 +73,40 @@
         }
         return false;
     }
-    /** Returns rational number that is this + other. */
+    /** Returns rational number that is this + other. 
+     * @param other RationalNumber object
+     * @return RationalNumber object after addition
+    */
     public RationalNumber add(RationalNumber other) {
         int numAdded = this.numerator * other.denominator + other.numerator * this.denominator;
         int denAdded = this.denominator * other.denominator;
         return new RationalNumber(numAdded, denAdded);
     }
 
-    /** Returns rational number that is this - other. */
+    /** Returns rational number that is this - other. 
+     * @param other RationalNumber object
+     * @return RationalNumber object after subtraction
+    */
     public RationalNumber subtract(RationalNumber other) {
         int numSubtracted = this.numerator * other.denominator - other.numerator * this.denominator;
         int denSubtracted = this.denominator * other.denominator;
         return new RationalNumber(numSubtracted, denSubtracted);
     }
 
-    /** Returns rational number that is this * other. */
+    /** Returns rational number that is this * other. 
+     * @param other RationalNumber object
+     * @return RationalNumber object after multiplication
+    */
     public RationalNumber multiply(RationalNumber other) {
         int numMultiplied = this.numerator * other.numerator;
         int denMultiplied = this.denominator * other.denominator;
         return new RationalNumber(numMultiplied, denMultiplied);
     }
 
-    /** Returns rational number that is this / other. */
+    /** Returns rational number that is this / other.
+     * @param other RationalNumber object
+     * @return RationalNumber object after division
+     */
     public RationalNumber divide(RationalNumber other) {
         int numDivided = this.numerator * other.denominator;
         int denDivided = this.denominator * other.numerator;
@@ -94,10 +116,11 @@
     /** Greatest common divisor
      * @param a integer
      * @param b integer
+     * @return integer gcd
      */
     public int getGreatestCommonDivisor(int a, int b) {
-        if (b==0) return Math.abs(a);
-        return getGreatestCommonDivisor(b,a%b);
-     }
+        if (b == 0) return Math.abs(a);
+        return getGreatestCommonDivisor(b,a % b);
+    }
 
- }
+}
